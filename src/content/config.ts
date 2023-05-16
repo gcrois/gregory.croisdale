8a786676a97814fb69bf372cb3364191712a0658
Gregory Croisdale <gregtc@umich.edu>
2023-05-16 00:54:32 -0400

    fancy grid?? you heard that right... 🤯

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,32 @@
 // @ts-ignore
 import { z, defineCollection } from 'astro:content';
 
+interface Cardable {
+    title: string;
+    description: string;
+    date: string;
+    tags: string[];
+    image?: string;
+}
+
 const schema = z.object({
     title: z.string(),
     description: z.string(),
     date: z.string(),
     tags: z.array(z.string()),
 
-    image: z.string().optional(),
+    image: z.string().default("/images/me.jpg"),
 });
 
 const blogCollection = defineCollection({
     schema: schema,
 });
 
+const researchCollection = defineCollection({
+    schema: schema,
+});
+
 export const collections = {
     'blog': blogCollection,
+    'research': researchCollection,
 };
---
+++ src/content/research/test2.md
@@ -0,0 +1,7 @@
+---
+title: "Test2"
+date: "2023-05-15"
+
+description: "This is a test research post to test the collection management in astro."
+tags: ["Test"]
+---
